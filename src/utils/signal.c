b0c0337de7078da6018a94c9bde4ce6b5237d486
lucas59356 <lucas59356@gmail.com>
2018-09-13 23:43:09 -0300

    [FaseInicial] Porte para o windows

--- src/utils/signal.c
+++ src/utils/signal.c
@@ -4,9 +4,13 @@
 #define NULL 0
 
 int catch_signal(int sig, void (*handler)(int)) {
+#ifndef WIN32
     struct sigaction action;
     action.sa_handler = handler;
     sigemptyset(&action.sa_mask);
     action.sa_flags = 0;
     return sigaction(sig, &action, NULL);
+#else
+    return signal(SIGINT, handler) ? 0: 1;
+#endif
 }
