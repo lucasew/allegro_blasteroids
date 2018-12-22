a975af025ff401c87acc2eee36f099247ea17779
lucas59356 <lucas59356@gmail.com>
2018-12-22 18:03:49 -0200

    Tirando um pedaço que agora não é mais necessário

--- include/blasteroids/pos_fixer.h
+++ include/blasteroids/pos_fixer.h
@@ -3,23 +3,6 @@
 
 #include <blasteroids/context.h>
 
-// Deixa os elementos dentro da janela
-#define INSIDE_SCREEN(obj, w, h) \
-    do { \
-        if(obj->sx > w) { \
-        obj->sx = 0; \
-        }; \
-        if (obj->sy > h) { \
-            obj->sy = 0; \
-        }; \
-        if(obj->sx < 0) { \
-            obj->sx = w; \
-        }; \
-        if(obj->sy < 0) {  \
-            obj->sy = h; \
-        }; \
-    } while (0);
-
 void blasteroids_fix_positions(GameContext *ctx);
 
 #endif
